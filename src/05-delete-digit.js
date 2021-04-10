c5247469f04eb859dca5a5812b3e0e0229855dd2
Sarter Wagner <hobitt.as@gmail.com>
2021-04-10 17:28:30 +0300

    feat: added 07 solution

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -1,14 +1,3 @@
-/**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For n = 152, the output should be 52
- *
- */
 function deleteDigit(n) {
   const arr = [];
   const numeric = String(n).split('');
--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,20 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  let mass = names;
+  let i = 1;
+  for (let j = 0, iter = 1, b; j < names.length; j++) {
+    b = mass[j];
+    mass = [].concat(mass.slice(0, i), mass.slice(i++).map((elem) => {
+      if (elem === b) {
+        return `${elem}(${iter++})`;
+      }
+      return elem;
+    }));
+    iter = 1;
+  }
+  return mass;
 }
 
 module.exports = renameFiles;
