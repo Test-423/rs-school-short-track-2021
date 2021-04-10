eecebcb44c10b85e4bd14c2a851c7fc9233c4f21
Sarter Wagner <hobitt.as@gmail.com>
2021-04-10 17:14:54 +0300

    feat: added 03 and 05 solution

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,15 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  let mass = [];
+  const end = [];
+  mass = arr.filter((elem) => elem !== -1).sort((a, b) => b - a);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) end[i] = -1;
+    else end[i] = mass.pop();
+  }
+  return end;
 }
 
 module.exports = sortByHeight;
--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,13 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = [];
+  const numeric = String(n).split('');
+  for (let i = 0; i < numeric.length; i++) {
+    arr.push(Number(numeric.filter((elem, index) => index !== i).join('')));
+  }
+  return Math.max(...arr);
 }
 
 module.exports = deleteDigit;
