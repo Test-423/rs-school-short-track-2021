98693941ed87cc0643d22f9cc5384c342cefb569
Sarter Wagner <hobitt.as@gmail.com>
2021-04-12 19:27:01 +0300

    feat: added 11 solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,21 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(n) {
+  let str = n.split('');
+  let res = '';
+  while (str.length !== 0) {
+    const elem = str[str.length - 1];
+    let index = 0;
+    for (let i = str.length - 1; i >= 0; i--) {
+      if (str[i] === elem) {
+        index++;
+        str = str.slice(0, -1);
+      } else break;
+    }
+    res += (index === 1) ? elem : elem + index;
+  }
+  return res.split('').reverse().join('');
 }
 
 module.exports = encodeLine;
